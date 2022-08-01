bad7b18547c8ecb86fbfb2623c6ced6ca2e602f1
Leonardo Franco <leofranco224@gmail.com>
2022-08-01 16:49:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'projetoTeste';
+  count = 0;
+  nome = ''
+  pessoas = ['leo', 'teste', 'teste1'];
+  variavel = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.nome = 'leo';
+    this.count = 5;
+    this.variavel = 0;
+    this.alteraVariavel();
+  }
+
+  alteraVariavel() {
+    return this.variavel++;
+  }
 }
